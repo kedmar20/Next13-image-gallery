7eeafa3602e326db25673c9e1037cc561f34e6d5
Kedmar20 <m.kedzierski76@gmail.com>
2023-09-20 06:02:19 +0200

    add fetching function on searchpage

--- src/app/search/SearchPage.tsx
+++ src/app/search/SearchPage.tsx
@@ -1,16 +1,33 @@
 "use client";
 
 import { Button, Form } from "react-bootstrap";
-import { FormEvent } from "react"; //for more complicated Form should be used useForm from React
+import { FormEvent, useState } from "react"; //for more complicated Form should be used useForm from React
 
 export default function SearchPageCC() {
+   const [searchResults, setSearchResults] = useState<UnsplashImage[] | null>(null);
+   const [searchResultsLoading, setSearchResultsLoading] = useState(false);
+   const [searchResultsLoadingIsError, setSearchResultsLoadingIsError] = useState(false);
+
    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
       e.preventDefault();
       const formData = new FormData(e.target as HTMLFormElement); //this is necessary to use with Form from React
       const query = formData.get("query")?.toString().trim();
 
-      if (query) {
-      }
+       if (query) {
+            try {
+                setSearchResults(null);
+                setSearchResultsLoadingIsError(false);
+                setSearchResultsLoading(true);
+                const response = await fetch("/api/search?query=" + query);
+                const images: UnsplashImage[] = await response.json();
+                setSearchResults(images);
+            } catch (error) {
+                console.error(error);
+                setSearchResultsLoadingIsError(true);
+            } finally {
+                setSearchResultsLoading(false);
+            }
+        
    }
 
    return (
@@ -24,6 +41,12 @@ export default function SearchPageCC() {
                SEARCH
             </Button>
          </Form>
+
+          <div className="d-flex flex-column align-items-center">
+                {searchResultsLoading && <Spinner animation="border" />}
+                {searchResultsLoadingIsError && <p>Something went wrong. Please try again.</p>}
+                {searchResults?.length === 0 && <p>Nothing found. Try a different query!</p>}
+            </div>
       </div>
    );
 }
